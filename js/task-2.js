29cecb1ce08cb1abc8990b9926df945e1e04cbaf
ViktoriiaBut <zironka.tory@gmail.com>
2024-11-07 23:12:21 +0000

    no

--- js/task-2.js
+++ js/task-2.js
@@ -11,7 +11,7 @@ class Storage {
       
     }
     removeItem(itemToRemove) {
-        this.#items = this.#items.filter(item => item !== itemToRemove);
+        
     }
 }
  const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
